11b64e8733f8aed1bdd88073a3cfb6e1903aa1c9
mferraris <mgl.ferraris@gmail.com>
2021-12-15 16:20:34 +0100

    added input validation + comments to clarify code

--- routers/characters.js
+++ routers/characters.js
@@ -5,60 +5,95 @@ const axios = require("axios");
 const { apiUrl } = require("../constants");
 
 router.get("/", async (req, res, next) => {
-  const { search, gender, sort, page } = req.query;
+  // 'search' query is used to find the characters of a specific film
+  // 'gender' query is used fo filter by 'male' or 'female'
+  // 'sortByAge' or 'sortByHeight' queries are used to sort based on 'asc' or 'desc' input
+  // 'page' query is used in the pagination function
+  const { search, gender, sortByAge, sortByHeight, page } = req.query;
 
   try {
+    // used let so variable can be changed depending on which queries are used
     let characterObjects = null;
 
     if (search) {
       const film = await axios.get(`${apiUrl}/films/?search=${search}`);
+
+      // validation in case search term is not included in any movie:
+      if (film.data.count === 0) {
+        return res
+          .status(400)
+          .send("Could not find movie with specified search term.");
+      }
+
+      // had to access 0 index in the results array to be able to map over character urls on line 32
       const charactersUrls = film.data.results[0].characters;
 
+      // making a request for each character url for specified film:
       const characterListRequests = charactersUrls.map((eachCharacterUrl) =>
         axios.get(eachCharacterUrl)
       );
 
+      // assigning previous requests to new const once they are ready:
       const characterObjectsData = await Promise.all(characterListRequests);
 
+      // assigning characters data to previously declared variable
       characterObjects = characterObjectsData.map(
         (eachCharacterData) => eachCharacterData.data
       );
     } else {
-      const allCharactersData = await axios.get(
-        `${apiUrl}/people/?page=${!page ? 1 : page}`
-      );
-
-      characterObjects = allCharactersData.data.results;
+      return res.status(400).send("Please specify movie search term");
     }
 
-    if (gender) {
+    // binary gender filter:
+    if (gender === "male" || gender === "female") {
       characterObjects = characterObjects.filter(
         (eachCharacterObject) => eachCharacterObject.gender === gender
       );
+    } else if (gender) {
+      return res
+        .status(400)
+        .send("Could not filter by gender. Specify 'male' or 'female'.");
     }
 
-    if (sort && sort === "heightAsc") {
+    // sorting by ascending or descending height:
+    if (sortByHeight && sortByHeight === "asc") {
       characterObjects.sort((a, b) => a.height - b.height);
-    } else if (sort && sort === "heightDesc") {
+    } else if (sortByHeight && sortByHeight === "desc") {
       characterObjects.sort((a, b) => b.height - a.height);
+    } else if (sortByHeight) {
+      return res
+        .status(400)
+        .send(
+          "Please specify 'asc' or 'desc' to sort by ascending or descending height."
+        );
     }
 
-    if (sort && sort === "ageAsc") {
+    // sorting by ascending or descending age:
+    if (sortByAge && sortByAge === "asc") {
       characterObjects.sort(
+        //had to use parseFloat since birth_year is a string with 'BBY' after the number
         (a, b) => parseFloat(a.birth_year) - parseFloat(b.birth_year)
       );
-    } else if (sort && sort === "ageDesc") {
+    } else if (sortByAge && sortByAge === "desc") {
       characterObjects.sort(
         (a, b) => parseFloat(b.birth_year) - parseFloat(a.birth_year)
       );
+    } else if (sortByAge) {
+      return res
+        .status(400)
+        .send(
+          "Please specify 'asc' or 'desc' to sort by ascending or descending age."
+        );
     }
 
+    // response sent according to pagination function:
     res.send(paginate(characterObjects, page));
   } catch (error) {
     next(error);
   }
 });
 
+// pagination function to limit 30 entries per page:
 function paginate(dataToSend, page) {
   const currentPage = !page ? 1 : page;
   const limit = 30;
--- routers/planets.js
+++ routers/planets.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 const { apiUrl } = require("../constants");
 
 router.get("/", async (req, res, next) => {
+  // 'climate' query is used to find all planets with a certain climate type
   const { climate, page } = req.query;
 
   try {
@@ -12,15 +13,16 @@ router.get("/", async (req, res, next) => {
       `${apiUrl}/planets/?page=${!page ? 1 : page}`
     );
 
+    // using let so variable can be changed if filter is applied
     let planetsArray = allPlanets.data.results;
 
     if (climate) {
       // filtering allPlanets array according to climate keyword input:
       const filteredPlanetsArray = planetsArray
         .filter((eachPlanet) =>
+          // had to split since 'climate' key comes as a string with all climate types:
           eachPlanet.climate.split(", ").includes(climate)
         )
-
         // mapping over filteredPlanetsArray to request each residents url:
         .map(async (eachPlanet) => {
           const residentsUrlsRequests = eachPlanet.residents.map(
@@ -48,7 +50,12 @@ router.get("/", async (req, res, next) => {
       planetsArray = await Promise.all(filteredPlanetsArray);
     }
 
-    res.send(planetsArray);
+    // data validation in case planetsArray returns empty:
+    if (planetsArray.length === 0) {
+      return res.status(400).send("Could not find specified climate type");
+    } else {
+      res.send(planetsArray);
+    }
   } catch (error) {
     next(error);
   }
