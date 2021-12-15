b6bc359cf768455e9339fd9ee72942e78396ec2a
mferraris <mgl.ferraris@gmail.com>
2021-12-15 13:13:34 +0100

    corrected filter planet by climate

--- routers/planets.js
+++ routers/planets.js
@@ -4,24 +4,48 @@ const axios = require("axios");
 
 const { apiUrl } = require("../constants");
 
-// fetch planets by climate search term
 router.get("/", async (req, res, next) => {
   const { climate, page } = req.query;
 
   try {
-    const planets = await axios.get(`${apiUrl}/planets`);
-    let planetsArray = planets.data.results;
+    const allPlanets = await axios.get(
+      `${apiUrl}/planets/?page=${!page ? 1 : page}`
+    );
+
+    let planetsArray = allPlanets.data.results;
 
     if (climate) {
-      planetsArray = planetsArray.filter((eachPlanet) =>
-        eachPlanet.climate.split(", ").includes(climate)
-      );
-    } else {
-      const allPlanets = await axios.get(
-        `${apiUrl}/planets/?page=${!page ? 1 : page}`
-      );
+      // filtering allPlanets array according to climate keyword input:
+      const filteredPlanetsArray = planetsArray
+        .filter((eachPlanet) =>
+          eachPlanet.climate.split(", ").includes(climate)
+        )
 
-      planetsArray = allPlanets.data.results;
+        // mapping over filteredPlanetsArray to request each residents url:
+        .map(async (eachPlanet) => {
+          const residentsUrlsRequests = eachPlanet.residents.map(
+            (eachResidentUrl) => axios.get(eachResidentUrl)
+          );
+          // defining a resident list after all requests are done:
+          const residentList = await Promise.all(residentsUrlsRequests);
+
+          // filtering dark haired residents from the resident list:
+          const darkHairedResidents = residentList
+            .map((eachResident) => eachResident.data)
+            .filter(
+              (eachResident) =>
+                eachResident.hair_color === "black" ||
+                eachResident.hair_color === "brown"
+            );
+
+          // returning a new object with the planet data and a new key containing the dark haired residents objects:
+          return {
+            ...eachPlanet,
+            dark_haired_residents: darkHairedResidents,
+          };
+        });
+
+      planetsArray = await Promise.all(filteredPlanetsArray);
     }
 
     res.send(planetsArray);
